24220361aec2b17c109cda4b23c4c3b183a5cb37
Eric Wood <epwood1@gmail.com>
2020-07-14 22:09:24 -0700

    MVP

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -38,6 +39,7 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+player = Player("Eric", room['outside'])
 
 # Write a loop that:
 #
@@ -49,3 +51,13 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+choice = "new"
+while choice != "q":
+    if choice == "Invalid choice":
+        print("Invalid choice: Please enter n, s, e, w, or q")
+    else:
+        player.printRoomName()
+        player.printDescription()
+    
+    choice = input("Enter command: ")
+    choice = player.move(choice)
--- src/player.py
+++ src/player.py
@@ -1,2 +1,58 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, name, current_room):
+        self.name = name
+        self.room = current_room
+
+    def moveNorth(self):
+        if(self.room.n_to):
+            self.room = self.room.n_to
+            print("You move North")
+        else:
+            print("You can't go that way.")
+
+    def moveSouth(self):
+        if(self.room.s_to):
+            self.room = self.room.s_to
+            print("You move South")
+        else:
+            print("You can't go that way.")
+
+    def moveEast(self):
+        if(self.room.e_to):
+            self.room = self.room.e_to
+            print("You move East")
+        else:
+            print("You can't go that way.")
+
+    def moveWest(self):
+        if(self.room.w_to):
+            self.room = self.room.w_to
+            print("You move West")
+        else:
+            print("You can't go that way.")
+    
+    def move(self, choice):
+        if(choice == "n"):
+            self.moveNorth()
+        elif(choice == "s"):
+            self.moveSouth()
+        elif(choice == "e"):
+            self.moveEast()
+        elif(choice == "w"):
+            self.moveWest()
+        elif(choice != "q"):
+            return "Invalid choice"
+
+        return choice
+
+
+    def printRoomName(self):
+        if(self.room):
+            print("You are " + self.room.printName())
+
+    def printDescription(self):
+        if(self.room):
+            print(self.room.printDescription())
